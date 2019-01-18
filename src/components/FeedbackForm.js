7614d47db84fc1836996e49580d12de83813a2a0
Vahan Mkrtumyan <vahanmkrtumyan@gmail.com>
2019-01-18 12:10:06 +0400

    18.1

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import * as emailjs from "emailjs-com";
+import "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
 
 class FeedbackForm extends Component {
   state = {
@@ -18,9 +20,8 @@ class FeedbackForm extends Component {
 
   handleInputChange = event => {
     event.preventDefault();
-    const target = event.target;
-    const name = event.name;
-    const value = target.value;
+    const name = event.target.name;
+    const value = event.target.value;
 
     this.setState({ [name]: value });
   };
@@ -56,8 +57,67 @@ class FeedbackForm extends Component {
 
   render() {
     return (
-      <div>
-        <form>
+      <div className="row">
+        <form className="col s6">
+          <div className="input-field col s6">
+            <input
+              id="last_name"
+              name="name"
+              type="text"
+              className="validate"
+              placeholder="Անուն"
+              onChange={this.handleInputChange}
+              value={this.state.name}
+            />
+            <label htmlFor="Անուն">Անուն</label>
+          </div>
+
+          <div className="input-field col s6">
+            <input
+              id="email"
+              type="email"
+              className="validate"
+              name="email"
+              onChange={this.handleInputChange}
+              value={this.state.email}
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <input
+                id="subject"
+                type="text"
+                className="validate"
+                name="subject"
+                onChange={this.handleInputChange}
+                value={this.state.subject}
+              />
+              <label htmlFor="subject">Թեմա</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6">
+              <div className="row">
+                <textarea
+                  id="textarea"
+                  className="materialize-textarea"
+                  onChange={this.handleInputChange}
+                  value={this.state.message}
+                  name="message"
+                />
+                <label htmlFor="textarea">Հաղորդագրություն</label>
+              </div>
+            </div>
+          </div>
+          <button onClick={this.sendMessage} type="button" name="submit">
+            Ուղարկել
+          </button>
+        </form>
+      </div>
+
+      /*  <div className="row">
+        <form className="col s6">
           <input
             type="text"
             name="name"
@@ -81,9 +141,9 @@ class FeedbackForm extends Component {
             onChange={this.handleInputChange}
             value={this.state.message}
           />
-          <button onClick={this.sendMessage} type="button" name="submit" />
+          <button onClick={this.sendMessage} type="button" name="submit" >Ուղարկել</button>
         </form>
-      </div>
+      </div> */
     );
   }
 }
