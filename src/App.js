3841419dbbccf5bd5f82c3042d77ac51a44aff96
Vahan Mkrtumyan <vahanmkrtumyan@gmail.com>
2019-01-22 19:03:12 +0400

    22.01-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./assets/scss/main.scss";
 // import FeedbackForm from './components/FeedbackForm';
 import PropTypes from "prop-types";
 import Homepage from "./components/Homepage";
 import CurrencyInput from "./CurrencyInput";
+import NotFound from './components/notFound';
 
 class App extends Component {
   render() {
@@ -15,7 +16,9 @@ class App extends Component {
         <div>
           <Switch>
             <Route path="/Kurs" component={CurrencyInput} />
-            <Route path="/" component={Homepage} />
+            <Route path="/" exact component={Homepage} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </React.Fragment>
