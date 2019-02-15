f67cd580de3462505c9c7a552063bd15cc111bff
Vahan Mkrtumyan <vahanmkrtumyan@gmail.com>
2019-02-15 19:05:52 +0400

    Update BannerComponent.jsx

--- src/components/homepage/BannerComponent.jsx
+++ src/components/homepage/BannerComponent.jsx
@@ -1,26 +1,30 @@
-import React, {Component} from 'react';
-import Fade from 'react-reveal/Fade';
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 class BannerComponent extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="banner">
-                    <div className="container">
-                        <Fade delay={300} cascade>
-                            <div className="banner-inner">
-
-                                <h1>ՈՂՋՈՒՅՆ <span>ՆԵՐԴՐՈՂ</span></h1>
-                                <p><span>Բրիդջ Կապիտալը </span> ներդրումային ընկերություն է, որի նպատակը բարձրակարգ ծառայությունների մատուցումն է</p>
-
-                            </div>
-                        </Fade>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="banner">
+          <div className="container">
+            <Fade delay={300} cascade>
+              <div className="banner-inner">
+                <h1>
+                  ՈՂՋՈՒՅՆ <span>ՆԵՐԴՐՈՂ</span>
+                </h1>
+                <p>
+                  <span>Բրիդջ Կապիտալը </span> ներդրումային ընկերություն է, որի
+                  առաքելությունն է մատուցել իր հաճախորդներին որակյալ և մշտապես
+                  հասանելի ներդրումային ծառայություններ,  ապահովել կայուն,
+                  զարգացող և երկարաժամկետ գործընկերային հարաբերություններ:
+                </p>
+              </div>
+            </Fade>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default BannerComponent;
-
